040dc8b1384494a6fbff07910677606b5570ea25
Masian Parkire <parkiremasian@gmail.com>
2023-02-16 05:47:05 +0300

    strings1

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,36 @@
+fun main(){
+    placename()
+println(details("Masian",21))
+    println(length(""))
+namecheck("IAN")
+    namecheck("Mercy")
+
+}
+fun placename(){
+    val place = "akirachix"
+    println(place[0])
+    println(place[2])
+    println(place[3])
+
+}
+fun details(name :String,age:Int) : String {
+val deets = " Hi, my name is $name and I am $age"
+    return deets
+}
+fun length(text: String) :Int{
+    val text = "My name is Masian and I am a girl"
+return text.length
+}
+fun namecheck(name:String) {
+    if (name == "Mercy") {
+        println("That's me!")
+    }else {
+        println("I don't know who that is")
+    }
+
+}
+
+
+
+
+
